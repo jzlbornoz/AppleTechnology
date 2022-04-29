d351e5ce197f23e5c1b7a08f57531f93ae854e1f
Javier Albornoz <javieraap10@gmail.com>
2022-04-29 12:24:35 -0400

    Se incluyo el mapa con leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import '../style/components/Map.css';
 
 const Map = () => {
     return (
-        <section><img src="https://staticmapmaker.com/img/google.png" alt="example" /></section>
+        <section className='Map-container'>
+            <MapContainer center={[8.597472, -71.150258]} zoom={12} scrollWheelZoom={false} id='map'>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[8.597472, -71.150258]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </section>
     )
 }
 
-export { Map }
+export { Map };
