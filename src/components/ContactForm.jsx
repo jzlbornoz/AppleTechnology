5937bee3a2a4e45ac852eeb8a4fae752d49920c3
Javier Albornoz <javieraap10@gmail.com>
2022-05-02 18:55:51 -0400

    Se completo la funcion con la api de whatsapp

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../style/components/ContactForm.css';
 
 const ContactForm = ({ setOpenModal }) => {
     const click = () => {
         setOpenModal(false);
     };
+    const [messege, setMessege] = useState('');
+    const onChange = (event) => {
+        setMessege(event.target.value);
+    };
+    const api = `https://api.whatsapp.com/send?phone=584121284000&text=${messege}`;
+
     return (
         <section className="ContactForm">
             <div className="Contact-wrapped">
-                <button type='button' onClick={click} className='Contact-exit'><i class="fa-solid fa-xmark" /></button>
+                <button type='button' onClick={click} className='Contact-exit'><i className="fa-solid fa-xmark" /></button>
                 <h2>Contactate con nosotros</h2>
-                <textarea type="text" placeholder='Dejanos tu mensaje'/>
-                <button type='button' className='Contact-send' onClick={click}>Enviar</button>
+                <textarea type="text" placeholder='Dejanos tu mensaje' value={messege} onChange={onChange} />
+                <a href={api}>
+                    <button type='button' className='Contact-send' onClick={click}>Enviar</button>
+                </a>
             </div>
         </section>
     )
