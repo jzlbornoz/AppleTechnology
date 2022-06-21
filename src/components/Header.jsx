85a161f754bdc0ee16da26fb65fb161957515de5
jzlbornoz <javieraap10@gmail.com>
2022-06-21 17:00:19 -0400

    [add]scrollTop

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,16 @@ import '../style/components/Header.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
+  const scrollTop = () => { window.scrollTo({ top: 0, behavior: 'smooth' }); };
+
+  // ---
   return (
     <section className="Header">
       <div className='Header-wrapped'>
-        <Link to='/'><div className='Header-logo'></div></Link>
+        <Link to='/'><div className='Header-logo' onClick={() => { scrollTop() }}></div></Link>
         <ul className="Header-menu">
-          <li><Link to='cursos'>Cursos </Link></li>
-          <li> <Link to='market'>Market</Link></li>
+          <li><Link to='cursos' onClick={() => { scrollTop() }}>Cursos </Link></li>
+          <li> <Link to='market' onClick={() => { scrollTop() }}>Market</Link></li>
         </ul>
       </div>
     </section>
